e852128f5f85324e8e3c3a57c29dd0acdad9aad0
kkrivitskaja <pratugames@gmail.com>
2022-04-26 19:36:18 +0300

    feat: add ImageBlock component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import {Component} from 'react'
+import { Component } from 'react'
+
+import ImageBlock from "./components/ImageBlock/ImageBlock"
 
 class App extends Component {
     render() {
-        return <h1>Store APP</h1>;
+        return (<><h1>Store APP</h1>
+            <ImageBlock/>
+        </> );
     }
 }
 
---
+++ src/components/ImageBlock/ImageBlock.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+
+// import './ImageBlock.module.css';
+
+const images = [
+    'https://cdn.shopify.com/s/files/1/0087/6193/3920/products/DD1381200_DEOA_2_720x.jpg?v=1612816087',
+    'https://cdn.shopify.com/s/files/1/0087/6193/3920/products/DD1381200_DEOA_1_720x.jpg?v=1612816087',
+    'https://cdn.shopify.com/s/files/1/0087/6193/3920/products/DD1381200_DEOA_3_720x.jpg?v=1612816087',
+    'https://cdn.shopify.com/s/files/1/0087/6193/3920/products/DD1381200_DEOA_5_720x.jpg?v=1612816087',
+    'https://cdn.shopify.com/s/files/1/0087/6193/3920/products/DD1381200_DEOA_4_720x.jpg?v=1612816087',
+];
+
+/**ImageSlider is an image slider with zoom effect on PDP
+ * props:
+ * productImages
+ * productName
+ */
+class ImageBlock extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            img: images[0],
+        };
+        this.imageRef = React.createRef();
+        this.imageRef.current = [];
+        this.hoverHandler = this.hoverHandler.bind(this);
+        this.addRefs = this.addRefs.bind(this);
+    }
+
+    hoverHandler(image, i) {
+        this.setState({
+            img: image,
+        });
+        this.imageRef.current[i].classList.add('slider-gallery__img-wrap-active');
+        for (let j = 0; j < images.length; j++) {
+            if (i !== j) {
+                this.imageRef.current[j].classList.remove('slider-gallery__img-wrap-active');
+            }
+        }
+    }
+
+    addRefs(element) {
+        if (element && !this.imageRef.current.includes(element)) {
+            this.imageRef.current.push(element);
+        }
+    }
+
+    render() {
+        return (
+            <div className="slider">
+                <div className="slider-gallery">
+                    {images.map((image, i) => (
+                        <div
+                            className={
+                                i === 0
+                                    ? 'slider-gallery__img-wrap-active'
+                                    : 'slider-gallery__img-wrap'
+                            }
+                            key={i}
+                            onMouseOver={() => this.hoverHandler(image, i)}
+                            ref={this.addRefs}
+                        >
+                            {/* add product name to props for img alt attribute*/}
+                            <img className="slider-gallery__img" src={image} alt="" />
+                        </div>
+                    ))}
+                </div>
+                <div className="slider-dynamic">
+                    
+                </div>
+            </div>
+        );
+    }
+}
+export default ImageBlock;
