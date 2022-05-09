7f0133df9d965a22c7d92f7550e3401e78afe6d6
kkrivitskaja <pratugames@gmail.com>
2022-05-09 21:32:24 +0300

    fix: conflicts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import { Component } from 'react';
 
+import ProductInfo from './components/ProductInfo/ProductInfo';
+
+const brand = 'Apollo';
+const name = 'Running Short';
+const price = 50.0;
+const description =
+    "Find stunning women's cocktail dresses and party dresses. Stand out in lace and metallic cocktail dresses and party dresses from all your favorite brands.";
+
 class App extends Component {
     render() {
         return (
             <>
-               Main page
+                <h1>Store APP</h1>
+                <ProductInfo brand={brand} name={name} price={price} description={description} />
             </>
         );
     }
