e91e6869f64573c0a06b898a25d932e7973c5112
kkrivitskaja <pratugames@gmail.com>
2022-05-09 22:01:52 +0300

    feat: add BaseButton component to ProductInfo

--- src/components/ProductInfo/ProductInfo.js
+++ src/components/ProductInfo/ProductInfo.js
@@ -1,24 +1,93 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
+import BaseButton from '../BaseButton/BaseButton';
 import './ProductInfo.css';
 
-/**ProductInfo is a component showing product details such as brand and product name, available sizes, price, description
+/** ProductInfo is a component showing product details such as brand and product name, available sizes, price, description
 props example
-    brand = 'Apollo';
-    name = 'Running Short';
-    price = 50.00;
-    description = "Find stunning women's cocktail dresses and party dresses. Stand out in lace and metallic cocktail dresses and party dresses from all your favorite brands.";
-  */
+name="Nike Air Huarache Le",
+inStock=true,
+description= "<p>Great sneakers for everyday use!</p>",
+category= "clothes",
+"attributes": [
+        {
+        "name": "Size",
+        "id": "Size",
+        "type": "text",
+        "items": [
+            {
+            "displayValue": "40",
+            "value": "40",
+            "id": "40"
+            },
+            {
+            "displayValue": "41",
+            "value": "41",
+            "id": "41"
+            },
+            {
+            "displayValue": "42",
+            "value": "42",
+            "id": "42"
+            },
+            {
+            "displayValue": "43",
+            "value": "43",
+            "id": "43"
+            }
+        ]
+        }
+    ],
+    brand= 'Nike x Stuss',
+    
+    "prices": [
+        {
+        "currency": {
+            "label": "USD",
+            "symbol": "$"
+        },
+        "amount": 144.69
+        },
+        {
+        "currency": {
+            "label": "GBP",
+            "symbol": "£"
+        },
+        "amount": 104
+        },
+        {
+        "currency": {
+            "label": "AUD",
+            "symbol": "A$"
+        },
+        "amount": 186.65
+        },
+        {
+        "currency": {
+            "label": "JPY",
+            "symbol": "¥"
+        },
+        "amount": 15625.24
+        },
+        {
+        "currency": {
+            "label": "RUB",
+            "symbol": "₽"
+        },
+        "amount": 10941.76
+        }
+    ]
+    */
 
 class ProductInfo extends Component {
     render() {
+        const { name, inStock=true, description, category, brand, price } = this.props;
         return (
             <div className="product-info">
                 <div className="product-info__name-wrapper">
-                    <span className="product-info__name single-card__name--semibold">
-                        {this.props.brand}
-                    </span>
-                    <span className="product-info__name">{this.props.name}</span>
+                    <span className="product-info__name single-card__name--semibold">{brand}</span>
+                    <span className="product-info__name">{name}</span>
                 </div>
                 <div className="product-info__size-wrapper">
                     <span className="product-info__size">SIZE:</span>
@@ -32,16 +101,27 @@ class ProductInfo extends Component {
                 <div className="product-info__purchase">
                     <span className="product-info__price">PRICE:</span>
                     <span className="product-info__price single-card__price--value">
-                        {this.props.price.toFixed(2)}
+                        {price.toFixed(2)}
                     </span>
-                    
                 </div>
+                <BaseButton>{inStock ? 'ADD TO CART' : 'OUT OF STOCK'}</BaseButton>
                 <div className="product-info__description-wrapper">
-                    <p className="product-info__description">{this.props.description}</p>
+                    <p className="product-info__description">
+                        {description}
+                    </p>
                 </div>
             </div>
         );
     }
 }
+
+ProductInfo.propTypes = {
+    name: PropTypes.string,
+    inStock: PropTypes.bool,
+    description: PropTypes.string,
+    category: PropTypes.string,
+    brand: PropTypes.string,
+    price: PropTypes.number,
+};
 
 export default ProductInfo;
