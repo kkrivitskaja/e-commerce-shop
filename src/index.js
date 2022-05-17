dee261ce2809b3ee184e6e4b27cd201aec3ed33b
kkrivitskaja <pratugames@gmail.com>
2022-05-17 20:17:01 +0300

    feat: connect app to Apollo GraphQl

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import ApolloClient, { InMemoryCache, gql } from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
 import App from './App';
+import './index.css';
+
+const client = new ApolloClient({
+    uri: 'http://localhost:4000/',
+    cache: new InMemoryCache(),
+});
+
+client
+    .query({
+        query: gql`
+            {
+                categories {
+                    name
+                    products {
+                        name
+                        id
+                    }
+                }
+            }
+        `,
+    })
+    .then((res) => console.log(res));
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
-
-
