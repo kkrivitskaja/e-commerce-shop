e7af935ca831f1ac3d194bc437d5a2705ed84aaf
kkrivitskaja <pratugames@gmail.com>
2022-05-18 21:23:30 +0300

    feat: connect app to the react Router Dom and add basic routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import { Component } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
+
+import ProductList from './pages/ProductList/ProductList';
+import Cart from './pages/Cart/Cart';
+import PageNotFound from './pages/PageNotFound/PageNotFound';
+
+const GET_CATEGORY = gql`
+    {
+        categories {
+            name
+        }
+    }
+`;
 
 class App extends Component {
     render() {
+        const { loading, error, categories } = this.props.data;
         return (
             <>
-               Main page
+                {loading && !error && !categories && <div>LOADING!!!!</div>}
+                {!loading && error && !categories && <div>{error}</div>}
+                {categories && !loading && !error && (
+                    <div className="App">
+                        <Routes>
+                            {categories.map((category) => (
+                                <Route
+                                    path={`/${category.name}`}
+                                    key={category.name}
+                                    element={<ProductList name={category.name} />}
+                                />
+                            ))}
+                            <Route path={'/cart'} element={<Cart />} />
+                            <Route path="*" element={<PageNotFound />} />
+                        </Routes>
+                    </div>
+                )}
             </>
         );
     }
 }
 
-export default App;
+export default graphql(GET_CATEGORY)(App);
