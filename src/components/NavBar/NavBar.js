44fda1a7519b1a270e5f427b1137cededb0fdc62
kkrivitskaja <pratugames@gmail.com>
2022-05-19 18:15:13 +0300

    feat: add NavBar component

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+import styles from './NavBar.module.css';
+
+class NavBar extends Component {
+    render() {
+        const categories = this.props.category;
+        return (
+            <>
+                <div className={styles['nav']}>
+                    {categories.map((category) => (
+                        <NavLink
+                            to={`/${category.name}`}
+                            key={category.name}
+                            className={({ isActive }) =>
+                                classnames(styles['nav-link'], {
+                                    [styles['nav-link--active']]: isActive,
+                                })
+                            }
+                        >
+                            {category.name.toUpperCase()}
+                        </NavLink>
+                    ))}
+                </div>
+            </>
+        );
+    }
+}
+
+NavBar.propTypes = {
+    categories: PropTypes.arrayOf(PropTypes.string),
+};
+
+export default NavBar;
