f6c2129cbab495121272ccd2fb992737a221bdd6
kkrivitskaja <pratugames@gmail.com>
2022-05-20 18:12:23 +0300

    feat: add buy-icon to the ProductCard component

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
+import { ReactComponent as BuyButton } from '../../assets/buy-icon.svg';
+
 import styles from './ProductCard.module.css';
 
 /** ProductInfo is a component on the PLP showing product details such as photo, brand and product name, price*/
@@ -10,18 +12,27 @@ class ProductCard extends Component {
         const { name, inStock, brand, prices, id, disabled, gallery } = this.props;
         return (
             <>
-                <div className={classnames(styles['card'], {
-                    [styles['card--disabled']]: disabled
-                })}>
-                    <div className={styles["card-image-wrapper"]}>
-                        <img src={gallery[0]} alt="" className={styles["card-image"]} />
-                        {!inStock && <div className={styles["card-image__stock"]}>out of stock</div>}
+                <div
+                    className={classnames(styles['card'], {
+                        [styles['card--disabled']]: disabled,
+                    })}
+                >
+                    <div className={styles['card-image-wrapper']}>
+                        <img src={gallery[0]} alt={name} className={styles['card-image']} />
+                        {!inStock && (
+                            <div className={styles['card-image__stock']}>out of stock</div>
+                        )}
                     </div>
-                    <div className={styles["card-info"]}>
-                        <div className={styles["card-info__name"]}>
+                    <div className={styles['card-info']}>
+                        <BuyButton
+                            className={classnames(styles['card-info__buy'], {
+                                [styles['card-info__buy--active']]: inStock,
+                            })}
+                        />
+                        <div className={styles['card-info__name']}>
                             {brand} {name}
                         </div>
-                        <div className={styles["card-info__price"]}>
+                        <div className={styles['card-info__price']}>
                             {prices[0].currency.symbol}
                             {prices[0].amount}
                         </div>
