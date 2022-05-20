0e9351024bbf89a6bc163b8b0811d1070d40a771
kkrivitskaja <pratugames@gmail.com>
2022-05-21 01:41:31 +0300

    fix:styles PagesCommonHeader component

--- src/components/PagesCommonHeader/PagesCommonHeader.js
+++ src/components/PagesCommonHeader/PagesCommonHeader.js
@@ -7,18 +7,26 @@ import { ReactComponent as Logo } from '../../assets/logo.svg';
 import { ReactComponent as Currency } from '../../assets/currency-icon.svg';
 import { ReactComponent as Cart } from '../../assets/cart-icon.svg';
 
-import './PagesCommonHeader.css';
+import styles from './PagesCommonHeader.module.scss';
 
 class PagesCommonHeader extends Component {
     render() {
         const categories = this.props.category;
         return (
             <>
-                <header className='header'>
-                    <NavBar category={categories} />
-                    <Logo />
-                    <Currency />
-                    <Cart/>
+                <header className={styles['header']}>
+                    <NavBar category={categories}  className='nav'/>
+                    <div className={styles['header-logo']}>
+                        <Logo />
+                    </div>
+                    <div className={styles['header-actions']}>
+                        <button className="header-actions__btn">
+                            <Currency />
+                        </button>
+                        <button className="header-actions__btn">
+                            <Cart />
+                        </button>
+                    </div>
                 </header>
                 <main>
                     <Outlet />
