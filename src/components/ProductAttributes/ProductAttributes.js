fdaa76e6f41e811ac16aaf71aa9dbcef86c5bc7a
kkrivitskaja <pratugames@gmail.com>
2022-05-25 19:23:46 +0300

    feat: refactor ProductAttributes component

--- src/components/ProductAttributes/ProductAttributes.js
+++ src/components/ProductAttributes/ProductAttributes.js
@@ -1,49 +1,57 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-import './ProductAttributes.css';
+import styles from './ProductAttributes.module.scss';
 
 class ProductAttributes extends Component {
     render() {
-        const { attributes } = this.props;
+        const { attribute } = this.props;
 
         return (
-            <div className="attributes">
-                {attributes.map((attribute) => {
-                    if (attribute.type === 'text') {
-                        return (
-                            <div key={attribute.id} className="attribute">
-                                <div className="text">{attribute.name}</div>
-                                <div className="container">
-                                    {attribute.items.map((item) => (
-                                        <button className="color-btn" key={item.id}>
-                                            {item.displayValue}
-                                        </button>
-                                    ))}
-                                </div>
-                            </div>
-                        );
-                    }
-                    if (attribute.type === 'swatch') {
-                        return (
-                            <div key={attribute.id} className="attribute">
-                                <div className="text">{attribute.name}</div>
-                                <div className="container">
-                                    {attribute.items.map((item) => (
-                                        <button
-                                            className="color-btn"
-                                            key={item.id}
-                                            style={{ backgroundColor: `${item.value}` }}
-                                            title={item.displayValue}
-                                        ></button>
-                                    ))}
-                                </div>
-                            </div>
-                        );
-                    }
-                })}
+            <div className={styles['attribute']}>
+                {console.log(attribute)}
+                <div className={styles['attribute-text']}>{attribute.name}</div>
+                <div className={styles['attribute-btn-wrapper']}>
+                    {attribute.items.map((item) => (
+                        <button
+                            key={item.id}
+                            style={
+                                attribute.type === 'swatch'
+                                    ? {
+                                        backgroundColor: `${item.value}`,
+                                        borderColor:
+                                            `${item.value}` !== '#FFFFFF'
+                                                ? 'transparent'
+                                                : '#000',
+                                    }
+                                    : {
+                                        backgroundColor: '#FFFFFF',
+                                        }
+                            }
+                            className={styles['attribute-btn']}
+                        >
+                            {attribute.type !== 'swatch' ? item.displayValue : null}
+                        </button>
+                    ))}
+                </div>
             </div>
         );
     }
 }
+
+ProductAttributes.propTypes = {
+    attribute: PropTypes.shape({
+        id: PropTypes.string,
+        items: PropTypes.arrayOf(
+            PropTypes.shape({
+                displayValue: PropTypes.string,
+                value: PropTypes.string,
+                id: PropTypes.string,
+            })
+        ),
+        name: PropTypes.string,
+        type: PropTypes.string,
+    }),
+};
 
 export default ProductAttributes;
