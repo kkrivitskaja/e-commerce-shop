149748399e3dcd1ef0fe6146f4ca06c3426c4d90
kkrivitskaja <pratugames@gmail.com>
2022-05-25 19:01:51 +0300

    feat: refactor code: add ProductAttributes component and delete unused code from ProductDetails component

--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -2,38 +2,32 @@ import React, { Component } from 'react';
 import DOMPurify from 'dompurify';
 import PropTypes from 'prop-types';
 
-import styles from "./ProductDetails.module.scss";
-
 import BaseButton from '../BaseButton/BaseButton';
+import ProductAttributes from '../ProductAttributes/ProductAttributes';
+
+import styles from './ProductDetails.module.scss';
 
 /**ProductDetails is a component showing product details such as brand and product name, available attributes, price, description
  */
 
 class ProductDetails extends Component {
     render() {
-        const { name, inStock = true, description, category, brand, price } = this.props;
+        const { name, inStock, description, brand, prices, attributes } = this.props;
         return (
             <div className="product-info">
                 <div className="product-info__name-wrapper">
                     <span className="product-info__name single-card__name--semibold">{brand}</span>
                     <span className="product-info__name">{name}</span>
                 </div>
-                <div className="product-info__size-wrapper">
-                    <span className="product-info__size">SIZE:</span>
-                    <div className="product-info__btn-wrapper">
-                        <button className="product-info__btn">XS</button>
-                        <button className="product-info__btn">S</button>
-                        <button className="product-info__btn">M</button>
-                        <button className="product-info__btn">L</button>
-                    </div>
-                </div>
+                <ProductAttributes attributes={attributes} />
                 <div className="product-info__purchase">
                     <span className="product-info__price">PRICE:</span>
                     <span className="product-info__price single-card__price--value">
-                        {price.toFixed(2)}
+                        {prices[0].currency.symbol}
+                        {prices[0].amount}
                     </span>
                 </div>
-                <BaseButton full disabled={!inStock}>
+                <BaseButton disabled={!inStock}>
                     {inStock ? 'ADD TO CART' : 'OUT OF STOCK'}
                 </BaseButton>
                 <div
@@ -50,9 +44,9 @@ ProductDetails.propTypes = {
     name: PropTypes.string,
     inStock: PropTypes.bool,
     description: PropTypes.string,
-    category: PropTypes.string,
     brand: PropTypes.string,
-    price: PropTypes.number,
+    prices: PropTypes.array,
+    attributes: PropTypes.array,
 };
 
 export default ProductDetails;
