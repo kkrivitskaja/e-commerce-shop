33849daf09e3ab16a54ed163d18da7f5b43e0731
kkrivitskaja <pratugames@gmail.com>
2022-05-25 18:31:45 +0300

    feat: add dompurify dependency, used a sanitizer DOMPurify.sanitize() to prevent XSS in the ProductDetails component

--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -1,30 +1,38 @@
 import React, { Component } from 'react';
+import DOMPurify from 'dompurify';
 
 /**ProductDetails is a component showing product details such as brand and product name, available attributes, price, description
-  */
+ */
 
 class ProductDetails extends Component {
     render() {
+        const { name, inStock = true, description, category, brand, price } = this.props;
         return (
             <div className="product-info">
                 <div className="product-info__name-wrapper">
-                    <span className="product-info__name single-card__name--semibold">
-                        {this.props.brand}
-                    </span>
-                    <span className="product-info__name">{this.props.name}</span>
+                    <span className="product-info__name single-card__name--semibold">{brand}</span>
+                    <span className="product-info__name">{name}</span>
                 </div>
                 <div className="product-info__size-wrapper">
                     <span className="product-info__size">SIZE:</span>
+                    <div className="product-info__btn-wrapper">
+                        <button className="product-info__btn">XS</button>
+                        <button className="product-info__btn">S</button>
+                        <button className="product-info__btn">M</button>
+                        <button className="product-info__btn">L</button>
+                    </div>
                 </div>
                 <div className="product-info__purchase">
                     <span className="product-info__price">PRICE:</span>
                     <span className="product-info__price single-card__price--value">
-                        {this.props.price}
+                        {price.toFixed(2)}
                     </span>
                 </div>
-                <div className="product-info__description-wrapper">
-                    <p className="product-info__description">{this.props.description}</p>
-                </div>
+                <div
+                    className="product-info__description"
+                    /**used a sanitizer DOMPurify.sanitize() to prevent XSS*/
+                    dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(description) }}
+                />
             </div>
         );
     }
