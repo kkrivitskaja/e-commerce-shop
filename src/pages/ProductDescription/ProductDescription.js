d8ab1ec9dfd6153a5c650196db322aeaaa491f84
kkrivitskaja <pratugames@gmail.com>
2022-05-25 21:52:36 +0300

    feat: add ProductDescription page

---
+++ src/pages/ProductDescription/ProductDescription.js
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+
+class ProductDescription extends Component {
+    render() {
+        return <>Single Product</>;
+    }
+}
+
+export default ProductDescription;
