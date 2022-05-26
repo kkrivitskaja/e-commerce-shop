b785a054ef5b9d705a875a6f1a5926fefd27c2c4
kkrivitskaja <pratugames@gmail.com>
2022-05-26 22:55:26 +0300

    fix: NavLinks in header

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import { Component } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { graphql } from 'react-apollo';
 
 import ProductList from './pages/ProductList/ProductList';
 import Cart from './pages/Cart/Cart';
 import PageNotFound from './pages/PageNotFound/PageNotFound';
 import PagesCommonHeader from './components/PagesCommonHeader/PagesCommonHeader';
-import { GET_CATEGORY } from './graphql/Queries';
+
 class App extends Component {
     render() {
-        const { categories } = this.props.data;
         return (
             <>
                 <div className="App">
                     <Routes>
-                        <Route path="/" element={<PagesCommonHeader category={categories} />}>
+                        <Route path="/" element={<PagesCommonHeader />}>
                             <Route path="/:categoryId" element={<ProductList />} />
                             <Route path="/:categoryId/:productId" element={<ProductList />} />
                             <Route path={'/cart'} element={<Cart />} />
@@ -27,4 +25,4 @@ class App extends Component {
     }
 }
 
-export default graphql(GET_CATEGORY)(App);
+export default App;
