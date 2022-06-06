714f3bbaacaed9c1123d81912d28d3f7f5287439
kkrivitskaja <pratugames@gmail.com>
2022-06-06 23:43:32 +0300

    feat: ScrollToTop for automatically scroll to the top of the page on each route change

--- src/App.js
+++ src/App.js
@@ -6,25 +6,29 @@ import Cart from './pages/Cart/Cart';
 import PageNotFound from './pages/PageNotFound/PageNotFound';
 import PagesCommonHeader from './components/PagesCommonHeader/PagesCommonHeader';
 import ProductDescription from './pages/ProductDescription/ProductDescription';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
+;
 
 class App extends Component {
     render() {
         return (
             <>
                 <div className="App">
-                    <Routes>
-                        <Route path="/" element={<PagesCommonHeader />}>
-                            <Route path="/" element={<Navigate replace to="/catalog/all" />} />
-                            <Route path="/catalog/:categoryId" element={<ProductList />} />
-                            <Route
-                                path="/catalog/:categoryId/:productId"
-                                element={<ProductDescription />}
-                            />
-                            <Route path={'/cart'} element={<Cart />} />
-                            <Route path="/404" element={<PageNotFound />} />
-                            <Route path="*" element={<Navigate replace to="/404" />} />
-                        </Route>
-                    </Routes>
+                    <ScrollToTop>
+                        <Routes>
+                            <Route path="/" element={<PagesCommonHeader />}>
+                                <Route path="/" element={<Navigate replace to="/catalog/all" />} />
+                                <Route path="/catalog/:categoryId" element={<ProductList />} />
+                                <Route
+                                    path="/catalog/:categoryId/:productId"
+                                    element={<ProductDescription />}
+                                />
+                                <Route path={'/cart'} element={<Cart />} />
+                                <Route path="/404" element={<PageNotFound />} />
+                                <Route path="*" element={<Navigate replace to="/404" />} />
+                            </Route>
+                        </Routes>
+                    </ScrollToTop>
                 </div>
             </>
         );
