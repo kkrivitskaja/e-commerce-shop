10be468ccd3f9630ea39dcef2706af6ee082dfbe
kkrivitskaja <pratugames@gmail.com>
2022-06-12 02:51:06 +0300

    feat: navigation to the category all and styles for PageNotFound component

--- src/pages/PageNotFound/PageNotFound.js
+++ src/pages/PageNotFound/PageNotFound.js
@@ -1,5 +1,9 @@
 import { Component } from 'react';
 
+import BaseButton from '../../components/BaseButton/BaseButton';
+import withRouter from '../../helpers/withRouter';
+import { closeModalWindow } from '../../views/modals/modalActions';
+
 import styles from './PageNotFound.module.scss';
 
 class PageNotFound extends Component {
@@ -11,10 +15,20 @@ class PageNotFound extends Component {
                     <h3>OOPS! PAGE NOT BE FOUND </h3>
                     Sorry but the page you are looking for does not exist, have been removed. name
                     changed or is temporarily unavailable
+                    <BaseButton
+                        onClick={() => {
+                            this.props.navigate(`/catalog/all`);
+                            closeModalWindow();
+                        }}
+                        secondary
+                        className={styles['modal-btn']}
+                    >
+                        back to catalog
+                    </BaseButton>
                 </div>
             </>
         );
     }
 }
 
-export default PageNotFound;
+export default withRouter(PageNotFound);
