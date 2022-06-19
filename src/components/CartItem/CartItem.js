efff06c7c86cd790ada9e14b6e452b6e8a5edeba
kkrivitskaja <pratugames@gmail.com>
2022-06-19 12:43:58 -0300

    fix: CartItem props types

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
 import classNames from 'classnames';
+import PropTypes, { instanceOf } from 'prop-types';
 
 import ProductAttributes from '../ProductAttributes/ProductAttributes';
 import PriceView from '../PriceView/PriceView';
 import CartItemSlider from '../CartItemSlider/CartItemSlider';
-import ShowProductAmount from '../ShowProductAmount/ShowProductAmount';
+import ProductCounter from '../ProductCounter/ProductCounter';
 import { ReactComponent as Close } from '../../assets/close-icon.svg';
 import { removeProduct } from '../../views/cart/cartActions';
+import { closeDropdownList } from '../../views/modals/modalActions';
+import withRouter from '../../helpers/withRouter';
 
 import styles from './CartItem.module.scss';
 
@@ -31,84 +33,106 @@ class CartItem extends Component {
     }
 
     render() {
-        const { amount, brand, name, prices, attributes, gallery, productUrl, selectedAttribute } =
-            this.props.product;
-        const { modal, overlay, cart } = this.props;
+        const { product, modal, overlay } = this.props;
 
         return (
             <>
-                <div className={styles['item-wrapper']}>
-                    <div className={styles['item-info']}>
-                        <NavLink to={productUrl}>
-                            <div
-                                className={classNames(styles['item-brand'], {
-                                    [styles['item-brand--overlay']]: overlay,
-                                })}
-                            >
-                                {brand}
-                            </div>
-                            <div
-                                className={classNames(styles['item-name'], {
-                                    [styles['item-name--overlay']]: overlay,
-                                })}
-                            >
-                                {name}
-                            </div>
-                        </NavLink>
+                <div
+                    className={classNames(styles['item'], {
+                        [styles['item--overlay']]: overlay,
+                    })}
+                >
+                    <div
+                        className={classNames(styles['item-info'], {
+                            [styles['item-info--overlay']]: overlay,
+                        })}
+                        onClick={() => {
+                            this.props.navigate(product.productUrl);
+                            closeDropdownList();
+                        }}
+                    >
+                        <div
+                            className={classNames(styles['item-brand'], {
+                                [styles['item-brand--overlay']]: overlay,
+                            })}
+                        >
+                            {product.brand}
+                        </div>
+                        <div
+                            className={classNames(styles['item-name'], {
+                                [styles['item-name--overlay']]: overlay,
+                            })}
+                        >
+                            {product.name}
+                        </div>
 
                         <div>
                             {overlay ? (
-                                <PriceView prices={prices} amount={amount} overlay />
+                                <PriceView
+                                    prices={product.prices}
+                                    amount={product.amount}
+                                    overlay
+                                />
                             ) : (
-                                <PriceView prices={prices} amount={amount} />
+                                <PriceView prices={product.prices} amount={product.amount} />
                             )}
                         </div>
                         <div className={styles['item-attributes']}>
                             {overlay
-                                ? attributes?.map((attribute) => (
+                                ? product.attributes?.map((attribute) => (
                                       <ProductAttributes
                                           key={attribute.id}
                                           attribute={attribute}
-                                          selectedAttribute={selectedAttribute.get(attribute.id)}
+                                          selectedAttribute={product.selectedAttribute.get(
+                                              attribute.id
+                                          )}
                                           overlay
                                           cart
                                       />
                                   ))
-                                : attributes?.map((attribute) => (
+                                : product.attributes?.map((attribute) => (
                                       <ProductAttributes
                                           key={attribute.id}
                                           attribute={attribute}
-                                          selectedAttribute={selectedAttribute.get(attribute.id)}
+                                          selectedAttribute={product.selectedAttribute.get(
+                                              attribute.id
+                                          )}
                                           cart
                                       />
                                   ))}
                         </div>
                     </div>
 
-                    <div className={styles['item-details']}>
-                        {!modal && !overlay && (
-                            <ShowProductAmount product={this.props.product} amount={amount} />
-                        )}
-                        {overlay && (
-                            <ShowProductAmount
-                                product={this.props.product}
-                                amount={amount}
-                                overlay
-                            />
-                        )}
+                    <div
+                        className={classNames(styles['item-details'], {
+                            [styles['item-details--overlay']]: overlay,
+                        })}
+                    >
+                        {!modal && !overlay && <ProductCounter product={product} />}
+                        {overlay && <ProductCounter product={product} overlay />}
                         {!this.state.isMobile && (
                             <div
                                 className={classNames(styles['item-details-slider-wrapper'], {
                                     [styles['item-details-slider-wrapper--overlay']]: overlay,
                                 })}
                             >
-                                <CartItemSlider gallery={gallery} name={name} />
+                                {overlay ? (
+                                    <CartItemSlider
+                                        gallery={product.gallery}
+                                        name={product.name}
+                                        overlay
+                                    />
+                                ) : (
+                                    <CartItemSlider gallery={product.gallery} name={product.name} />
+                                )}
                             </div>
                         )}
                         {!modal && (
                             <button
-                                onClick={() => removeProduct(this.props.product)}
-                                className={styles['item-details-btn']}
+                                onClick={() => removeProduct(product)}
+                                className={classNames(styles['item-details-btn'], {
+                                    [styles['item-details-btn--overlay']]: overlay,
+                                })}
                             >
                                 <Close width={15} height={15} />
                             </button>
@@ -119,4 +143,45 @@ class CartItem extends Component {
         );
     }
 }
-export default CartItem;
+
+CartItem.propTypes = {
+    product: PropTypes.shape({
+        amount: PropTypes.number,
+        attributes: PropTypes.arrayOf(
+            PropTypes.shape({
+                id: PropTypes.string,
+                items: PropTypes.arrayOf(
+                    PropTypes.shape({
+                        displayValue: PropTypes.string,
+                        value: PropTypes.string,
+                        id: PropTypes.string,
+                    })
+                ),
+                name: PropTypes.string,
+                type: PropTypes.string,
+            })
+        ),
+        brand: PropTypes.string,
+        gallery: PropTypes.arrayOf(PropTypes.string),
+        id: PropTypes.string,
+        inStock: PropTypes.bool,
+        name: PropTypes.string,
+        prices: PropTypes.arrayOf(
+            PropTypes.shape({
+                currency: PropTypes.shape({
+                    label: PropTypes.string,
+                    symbol: PropTypes.string,
+                }),
+                amount: PropTypes.number,
+            })
+        ),
+        productId: PropTypes.string,
+        productUrl: PropTypes.string,
+        selectedAttribute: instanceOf(Map),
+    }),
+
+    modal: PropTypes.bool,
+    overlay: PropTypes.bool,
+};
+
+export default withRouter(CartItem);
